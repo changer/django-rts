4f83a9f3164565d0ff4ca8e51901ab2c0c1c58eb
Jos van Velzen <jos.vanvelzen@changer.nl>
2015-06-12 12:36:43 +0200

    add response generator logic

--- rts/response_generators.py
+++ rts/response_generators.py
@@ -1,9 +1,12 @@
+from django.http import HttpResponse
+
+
 class ResponseGenerator(object):
     serializer_class = None
     serializer_kwargs = {}
 
-    def get_response(self, request):
-        raise NotImplementedError('.get_response(request) must be implemented')
+    def get_response(self, obj):
+        raise NotImplementedError('.get_response(obj) must be implemented')
 
 
 class WebFormAutoSubmitGenerator(ResponseGenerator):
@@ -11,7 +14,10 @@ class WebFormAutoSubmitGenerator(ResponseGenerator):
     field = 'data'
     action = None
 
-    def get_response(self, request, obj):
+    def get_response(self, obj):
         serializer = self.serializer_class()
 
         data = serializer.serialize(obj, **self.serializer_kwargs)
+        response = HttpResponse(data, content_type='text/xml')
+
+        return response
--- rts/views.py
+++ rts/views.py
@@ -5,12 +5,16 @@ from django.views.generic import View
 class RtsView(View):
     request_handler_class = None
     transformer_class = None
+    response_generator_class = None
 
     def dispatch(self, request, *args, **kwargs):
         request_handler = self.request_handler_class()
         transformer = self.transformer_class()
+        response_generator = self.response_generator_class()
 
         obj = request_handler.get_object(request)
         obj = transformer.transform(obj)
 
-        return HttpResponse(obj)
+        response = response_generator.get_response(obj)
+
+        return response
